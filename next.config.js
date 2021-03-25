7be6c2012245aeeef0e2c12f71e309632fcc7751
Jacob Aulenback <jacob@aulenback.ca>
2021-03-25 01:54:42 -0700

    add trailing slashes fix

--- next.config.js
+++ next.config.js
@@ -7,5 +7,6 @@ const withMDX = require("@next/mdx")({
   });
   
   module.exports = withMDX({
-    pageExtensions: ["js", "jsx", "md", "mdx"]
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+    trailingSlash: true 
   });
